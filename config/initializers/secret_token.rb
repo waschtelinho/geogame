b4b165738f311a3c145fab782d5ada73095dff1a
Basti <basti@wurscht.(none)>
2011-06-18 13:49:36 +0200

    inti

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geogame::Application.config.secret_token = '3fc300ddea4d88910027f573055fd857ed20c1c047c79c647e48f043d92a9fd8ff8ceb707aa0ea8e4424688bb5bf82f7376521f2974bfc8b2c2959de52afd247'
