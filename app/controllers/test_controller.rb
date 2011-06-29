b2352ffa0e77d056324f503743e1eef1b199c02e
Bastian Schumacher <bsch@kupferwerk.net>
2011-06-29 17:39:13 +0200

    random countries in js

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,6 +4,7 @@ class TestController < ApplicationController
 
 	def index
 		@map_information = MapCreator.get_map_information('country_borders.txt', 3)
+		@country_list = @map_information[:countries].collect {|country| country[0]}
 	end
 	
 end
