c536ff9b9faa597c45b80b2ab1eee7e4b9f6fbca
Bastian Schumacher <bsch@kupferwerk.net>
2011-12-28 14:13:16 +0100

    german works

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.6'
 gem 'sqlite3'
 gem 'rmagick'
+gem 'log_buddy'
 
 gem 'elecnix-ruby-geonames', '0.2.6', :path => 'vendor/gems/elecnix-ruby-geonames-0.2.6'
--- app/controllers/country_finder_controller.rb
+++ app/controllers/country_finder_controller.rb
@@ -5,9 +5,9 @@ class CountryFinderController < ApplicationController
   def index
     @map_information = {}
     MapCreator.zooms.each do |zoom|
-      @map_information[zoom] = MapCreator.get_map_information(:en, zoom)
+      @map_information[zoom] = MapCreator.get_map_information(:de, zoom)
     end
-    @country_list = (@map_information[1][:countries].collect {|country| country[0]}).uniq!
+    @country_list = (@map_information[1][:countries].collect {|country| country[0]}).uniq
   end
 
 end
